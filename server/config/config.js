3b80785582545dff56b10e1f347ff671a5f8bb3d
rulopper <joseraul.muve@gmail.com>
2018-11-22 22:22:53 -0600

    RestServer - Usuario

---
+++ server/config/config.js
@@ -0,0 +1,23 @@
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:Accespoint0@ds161856.mlab.com:61856/cafe';
+}
+process.env.URLDB = urlDB;

d477a0e142f3e4f4b9311c889e3d705471bf9966
rulopper <joseraul.muve@gmail.com>
2018-11-22 23:15:57 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:Accespoint0@ds161856.mlab.com:61856/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;

ec7575ce5818dadabbd54eac7edf2015ce6bf97e
rulopper <joseraul.muve@gmail.com>
2018-11-26 22:40:54 -0600

    Login normal

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,60 @@
+const jwt = require('jsonwebtoken');
+
+// ============================
+//  Verificar Token 
+// ============================
+
+
+let verificaToken = (req, res, next) => {
+    let token = req.get('token');
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token inválido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+
+
+};
+
+// ============================
+//  Verificar AdminRole
+// ============================
+
+
+let verificaAdmin_Role = (req, res, next) => {
+
+    let usuario = req.usuario;
+
+    if (usuario.role === 'ADMIN_ROLE') {
+        next();
+    } else {
+        res.json({
+            ok: false,
+            err: {
+                message: 'El usuario no es administrador'
+            }
+        });
+    }
+
+    res.json({
+        ok: false,
+        err: {
+            message: 'El usuario no es administrador'
+        }
+    })
+
+};
+
+module.exports = {
+    verificaToken,
+    verificaAdmin_Role
+}

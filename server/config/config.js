9e7c67e4eef5c14e760e9f242828b7b48b592ba8
rulopper <joseraul.muve@gmail.com>
2018-11-27 01:32:08 -0600

    Google Sign In

--- server/config/config.js
+++ server/config/config.js
@@ -31,3 +31,10 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
+
+
+// ============================
+//  Google Client ID
+// ============================
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '817728424749-8ojnntfkmjst833hu0qn7dpil105llqh.apps.googleusercontent.com';
